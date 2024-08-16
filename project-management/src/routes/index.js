56bce41f3c8a93db6bb185e628170c031b9e3d12
tkophailuongtienanh <tienanhluong01@gmail.com>
2024-08-16 11:47:03 +0700

    thêm url vào side bar

--- project-management/src/routes/index.js
+++ project-management/src/routes/index.js
@@ -1,15 +1,16 @@
-import { lazy } from 'react'
+import { lazy } from "react";
 
 // use lazy for better code splitting, a.k.a. load faster
-const Dashboard = lazy(() => import('../pages/Dashboard'))
-const Forms = lazy(() => import('../pages/Forms'))
-const Cards = lazy(() => import('../pages/Cards'))
-const Charts = lazy(() => import('../pages/Charts'))
-const Buttons = lazy(() => import('../pages/Buttons'))
-const Modals = lazy(() => import('../pages/Modals'))
-const Tables = lazy(() => import('../pages/Tables'))
-const Page404 = lazy(() => import('../pages/404'))
-const Blank = lazy(() => import('../pages/Blank'))
+const Dashboard = lazy(() => import("../pages/Dashboard"));
+const Team = lazy(() => import("../pages/Team/TeamManagement"));
+const Forms = lazy(() => import("../pages/Forms"));
+const Cards = lazy(() => import("../pages/Cards"));
+const Charts = lazy(() => import("../pages/Charts"));
+const Buttons = lazy(() => import("../pages/Buttons"));
+const Modals = lazy(() => import("../pages/Modals"));
+const Tables = lazy(() => import("../pages/Tables"));
+const Page404 = lazy(() => import("../pages/404"));
+const Blank = lazy(() => import("../pages/Blank"));
 
 /**
  * ⚠ These are internal routes!
@@ -23,41 +24,45 @@ const Blank = lazy(() => import('../pages/Blank'))
  */
 const routes = [
   {
-    path: '/dashboard', // the url
+    path: "/dashboard", // the url
     component: Dashboard, // view rendered
   },
   {
-    path: '/forms',
+    path: "/team", // the url
+    component: Team, // view rendered
+  },
+  {
+    path: "/forms",
     component: Forms,
   },
   {
-    path: '/cards',
+    path: "/cards",
     component: Cards,
   },
   {
-    path: '/charts',
+    path: "/charts",
     component: Charts,
   },
   {
-    path: '/buttons',
+    path: "/buttons",
     component: Buttons,
   },
   {
-    path: '/modals',
+    path: "/modals",
     component: Modals,
   },
   {
-    path: '/tables',
+    path: "/tables",
     component: Tables,
   },
   {
-    path: '/404',
+    path: "/404",
     component: Page404,
   },
   {
-    path: '/blank',
+    path: "/blank",
     component: Blank,
   },
-]
+];
 
-export default routes
+export default routes;
--- project-management/src/routes/sidebar.js
+++ project-management/src/routes/sidebar.js
@@ -7,67 +7,72 @@
  */
 const routes = [
   {
-    path: '/app/dashboard', // the url
-    icon: 'HomeIcon', // the component being exported from icons/index.js
-    name: 'Dashboard', // name that appear in Sidebar
+    path: "/app/dashboard", // the url
+    icon: "HomeIcon", // the component being exported from icons/index.js
+    name: "Dashboard", // name that appear in Sidebar
   },
   {
-    path: '/app/forms',
-    icon: 'FormsIcon',
-    name: 'Forms',
+    path: "/app/team", // the url
+    icon: "PeopleIcon", // the component being exported from icons/index.js
+    name: "Phòng ban", // name that appear in Sidebar
   },
   {
-    path: '/app/cards',
-    icon: 'CardsIcon',
-    name: 'Cards',
+    path: "/app/forms",
+    icon: "FormsIcon",
+    name: "Forms",
   },
   {
-    path: '/app/charts',
-    icon: 'ChartsIcon',
-    name: 'Charts',
+    path: "/app/cards",
+    icon: "CardsIcon",
+    name: "Cards",
   },
   {
-    path: '/app/buttons',
-    icon: 'ButtonsIcon',
-    name: 'Buttons',
+    path: "/app/charts",
+    icon: "ChartsIcon",
+    name: "Charts",
   },
   {
-    path: '/app/modals',
-    icon: 'ModalsIcon',
-    name: 'Modals',
+    path: "/app/buttons",
+    icon: "ButtonsIcon",
+    name: "Buttons",
   },
   {
-    path: '/app/tables',
-    icon: 'TablesIcon',
-    name: 'Tables',
+    path: "/app/modals",
+    icon: "ModalsIcon",
+    name: "Modals",
   },
   {
-    icon: 'PagesIcon',
-    name: 'Pages',
+    path: "/app/tables",
+    icon: "TablesIcon",
+    name: "Tables",
+  },
+  {
+    icon: "PagesIcon",
+    name: "Pages",
     routes: [
       // submenu
       {
-        path: '/login',
-        name: 'Login',
+        path: "/login",
+        name: "Login",
       },
       {
-        path: '/create-account',
-        name: 'Create account',
+        path: "/create-account",
+        name: "Create account",
       },
       {
-        path: '/forgot-password',
-        name: 'Forgot password',
+        path: "/forgot-password",
+        name: "Forgot password",
       },
       {
-        path: '/app/404',
-        name: '404',
+        path: "/app/404",
+        name: "404",
       },
       {
-        path: '/app/blank',
-        name: 'Blank',
+        path: "/app/blank",
+        name: "Blank",
       },
     ],
   },
-]
+];
 
-export default routes
+export default routes;
