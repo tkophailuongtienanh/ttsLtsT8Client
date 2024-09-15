919bc2de4eebf3b9b9907eabf40ef33d88a9328f
tkophailuongtienanh <tkophailuongtienanh@users.noreply.github.com>
2024-09-15 00:29:24 +0000

    deploy: d5ca2136cdec62786694d4c6cc18294b9631d9c0

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.60a72207d2e4ccf6b8d472ba0c42e2a8.js"
+  "/ttsLtsT8Client/precache-manifest.ea44389fbe89db9a8dfeac85ea5398ad.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ttsLtsT8Client/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
